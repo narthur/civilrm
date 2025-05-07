cd4c99ab2331041d4434ebc8451b2cb0a5b97e35
Nathan Arthur <nathan@nathanarthur.com>
2025-05-07 09:25:28 -0400

    add followups page

---
+++ components/followups/FollowupList.tsx
@@ -0,0 +1,7 @@
+export function FollowupList() {
+  return (
+    <div>
+      <p>Follow-up list placeholder. This feature is under construction.</p>
+    </div>
+  );
+}
---
+++ pages/followups.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import { FollowupList } from "../components/followups/FollowupList";
+
+const FollowupsPage: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold text-gray-900 mb-6">Follow-ups</h1>
+      <FollowupList />
+    </div>
+  );
+};
+
+export default FollowupsPage;
