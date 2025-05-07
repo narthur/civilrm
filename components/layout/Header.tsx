1c7c65e69589814ea18fb607def7ac7284d9c39b
Nathan Arthur <nathan@nathanarthur.com>
2025-05-07 10:46:25 -0400

    styling

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,14 +1,19 @@
-import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react";
+import {
+  UserButton,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
 import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="bg-white shadow">
+    <header className="bg-blue-900 shadow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <Link href="/" className="text-xl font-bold text-gray-800">
+              <Link href="/" className="text-xl font-bold text-white">
                 CivilRM
               </Link>
             </div>
